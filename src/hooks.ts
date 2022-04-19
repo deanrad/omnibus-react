38ff7ca94b76ddf9fec1e91f847a48cf7c8e1c49
Dean Radcliffe <24406+deanrad@users.noreply.github.com>
2022-04-19 17:51:52 -0500

    0.3.3 useServiceState and useDistinctServiceState

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useEffect } from 'react';
 import { Observable, Subscription } from 'rxjs';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 /**
  * Ties a subscription to the lifetime of the component that calls useWhileMounted.
@@ -25,6 +26,43 @@ export function useWhileMounted(
           (teardown as () => void)?.();
         };
   }, []);
+}
+
+/** Calls the function given on each new state of the service. Does not dedupe states!
+ * Unsubscribes on unmount. Only pass a setter that is stable!
+ * @param service The service to subscribe to
+ * @param setter The function which sets the new state on chnges
+ */
+export function useServiceState<T>(
+  service: { state: Observable<T> },
+  setter: (newState: T) => void
+) {
+  useWhileMounted(() =>
+    service.state.subscribe({
+      next(newState) {
+        setter(newState);
+      },
+    })
+  );
+}
+
+/** Calls the function given on each new state of the service, deduping with optional comparator.
+ * Unsubscribes on unmount. Only pass a setter that is stable!
+ * @param service The service to subscribe to
+ * @param setter The function which sets the new state on chnges
+ */
+export function useDistinctServiceState<T>(
+  service: { state: Observable<T> },
+  setter: (newState: T) => void,
+  comparator?: (previous: T, current: T) => boolean
+) {
+  useWhileMounted(() =>
+    service.state.pipe(distinctUntilChanged(comparator)).subscribe({
+      next(newState) {
+        setter(newState);
+      },
+    })
+  );
 }
 
 /**
--- test/hooks.spec.tsx
+++ test/hooks.spec.tsx
@@ -1,142 +1,161 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { useWhileMounted } from '../src/hooks';
+import { useWhileMounted, useServiceState } from '../src/hooks';
 import { Observable, Subscription } from 'rxjs';
-
 
 describe('useWhileMounted', () => {
   describe('called with a Subscription factory', () => {
-    let count = 0
+    let count = 0;
     const Example = () => {
       useWhileMounted(() => {
-        count++
-        return new Subscription(() => { count-- })
-      })
-      return null
-    }
+        count++;
+        return new Subscription(() => {
+          count--;
+        });
+      });
+      return null;
+    };
 
     describe('at mount time', () => {
       it('calls the factory', () => {
-        const result = render(<Example />)
-        expect(count).toEqual(1)
-      })
-    })
+        const result = render(<Example />);
+        expect(count).toEqual(1);
+      });
+    });
     describe('at unmount time', () => {
       it('unsubscribes the subscription', () => {
-        const { unmount } = render(<Example />)
-        expect(count).toEqual(1)
-        unmount()
-        expect(count).toEqual(0)
-      })
-    })
-  })
+        const { unmount } = render(<Example />);
+        expect(count).toEqual(1);
+        unmount();
+        expect(count).toEqual(0);
+      });
+    });
+  });
   describe('called with an EffectCallback', () => {
-    let count = 0
+    let count = 0;
     const Example = () => {
       useWhileMounted(() => {
-        count++
-        return () => { count-- }
-      })
-      return null
-    }
+        count++;
+        return () => {
+          count--;
+        };
+      });
+      return null;
+    };
 
     describe('at mount time', () => {
       it('calls the factory', () => {
-        const result = render(<Example />)
-        expect(count).toEqual(1)
-      })
-    })
+        const result = render(<Example />);
+        expect(count).toEqual(1);
+      });
+    });
     describe('at unmount time', () => {
       it('unsubscribes the subscription', () => {
-        const { unmount } = render(<Example />)
-        expect(count).toEqual(1)
-        unmount()
-        expect(count).toEqual(0)
-      })
-    })
+        const { unmount } = render(<Example />);
+        expect(count).toEqual(1);
+        unmount();
+        expect(count).toEqual(0);
+      });
+    });
     describe('with no teardown', () => {
-      let count = 0
+      let count = 0;
       const Example = () => {
         useWhileMounted(() => {
-          count++
-        })
-        return null
-      }
-      beforeEach(() => {count = 0})
-  
+          count++;
+        });
+        return null;
+      };
+      beforeEach(() => {
+        count = 0;
+      });
+
       describe('at mount time', () => {
         it('calls the factory', () => {
-          const result = render(<Example />)
-          expect(count).toEqual(1)
-        })
-      })
+          const result = render(<Example />);
+          expect(count).toEqual(1);
+        });
+      });
       describe('at unmount time', () => {
         it('doesnt err', () => {
-          const { unmount } = render(<Example />)
-          expect(count).toEqual(1)
-          unmount()
-        })
-      })
-    })
-  
-  })
+          const { unmount } = render(<Example />);
+          expect(count).toEqual(1);
+          unmount();
+        });
+      });
+    });
+  });
   describe('called with an Observable', () => {
-    let count = 0
+    let count = 0;
     const Example = () => {
       useWhileMounted(() => {
         return new Observable(() => {
           count++;
-          return () => { count-- }
-        })
-      })
-      return null
-    }
+          return () => {
+            count--;
+          };
+        });
+      });
+      return null;
+    };
 
     describe('at mount time', () => {
       it('calls the factory', () => {
-        const result = render(<Example />)
-        expect(count).toEqual(1)
-      })
-    })
+        const result = render(<Example />);
+        expect(count).toEqual(1);
+      });
+    });
     describe('at unmount time', () => {
       it('unsubscribes the subscription', () => {
-        const { unmount } = render(<Example />)
-        expect(count).toEqual(1)
-        unmount()
-        expect(count).toEqual(0)
-      })
-    })
-  })
+        const { unmount } = render(<Example />);
+        expect(count).toEqual(1);
+        unmount();
+        expect(count).toEqual(0);
+      });
+    });
+  });
 
   describe('With Mocks', () => {
     describe('called with a Subscription factory', () => {
-      const mockSub = new Subscription()
-      const mockUnsub = jest.spyOn(mockSub, 'unsubscribe')
-      const mockSubFactory = jest.fn(() => mockSub) as unknown as () => Subscription
+      const mockSub = new Subscription();
+      const mockUnsub = jest.spyOn(mockSub, 'unsubscribe');
+      const mockSubFactory = jest.fn(
+        () => mockSub
+      ) as unknown as () => Subscription;
       const Example = () => {
-        useWhileMounted(mockSubFactory)
-        return null
-      }
+        useWhileMounted(mockSubFactory);
+        return null;
+      };
 
       describe('at mount time', () => {
         it('calls the factory', () => {
-          const result = render(<Example />)
-          expect(mockSubFactory).toHaveBeenCalled()
-        })
-      })
+          const result = render(<Example />);
+          expect(mockSubFactory).toHaveBeenCalled();
+        });
+      });
       describe('at unmount time', () => {
         it('calls unsubscribe', () => {
-          const { unmount } = render(<Example />)
-          unmount()
-          expect(mockUnsub).toHaveBeenCalled()
-        })
-      })
-    })
-
-  })
+          const { unmount } = render(<Example />);
+          unmount();
+          expect(mockUnsub).toHaveBeenCalled();
+        });
+      });
+    });
+  });
 
   it('exists', () => {
     expect(useWhileMounted).toBeTruthy();
   });
+});
+
+describe('useServiceState', () => {
+  it.todo(
+    'calls the setter provided when the service emits a new state (even if the same as the old)'
+  );
+});
+
+describe('useDistinctServiceState', () => {
+  it.todo(
+    'calls the setter provided when the service emits a new state distinct from the old'
+  );
 });
